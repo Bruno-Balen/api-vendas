ce71105e3a7b84504095856b89dfec4affb0d0bf
Bruno Balen <brunobalen70@gmail.com>
2025-05-08 15:56:26 -0300

    Correção

--- src/routers/vendaProduto.js
+++ src/routers/vendaProduto.js
@@ -5,8 +5,4 @@ module.exports = (app) => {
   app.post('/vendas/:id/produtos', controller.PostVendaProduto);
   app.delete('/vendas/:id/produtos/:idproduto', controller.DeleteVendaProduto);
   app.put('/vendas/:id/produtos/:idproduto', controller.PutVendaProduto);
-//   app.post('/vendas-produtos', controller.PostVendasProdutos);
-//   app.get('/vendas-produtos/:id', controller.GetVendasProdutosById);
-//   app.put('/vendas-produtos/:id', controller.PutVendasProdutos);
-//   app.delete('/vendas-produtos/:id', controller.DeleteVendasProdutos);
 }
